470a0251839231c609ba3c92a0982a46ee72a86d
emdevelopa <gyimahsunday@gmail.com>
2024-02-29 07:14:01 +0100

    cool

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   define: {
-    'process.env': {},
+    "process.env": {},
   },
   build: {
-    chunkSizeWarningLimit: 50000
+    chunkSizeWarningLimit: 50000,
   },
   server: {
     host: true,
-  }
-})
+  },
+  
+});
